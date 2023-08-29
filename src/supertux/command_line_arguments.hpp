Rami-Slicer/rami_supertux_fork

Add --from-datadir command line option Only load from physfs path if --from-datadir is specified

src/supertux/command_line_arguments.hpp
@@ -80,6 +80,7 @@ public:
   boost::optional<std::string> repository_url;
 
   boost::optional<bool> editor;
+  boost::optional<bool> from_datadir;
   boost::optional<bool> resave;
 
   // boost::optional<std::string> locale;

syvb/supertux

src/supertux/command_line_arguments.hpp
@@ -78,8 +78,8 @@ public:
   std::optional<bool> christmas_mode;
 
   std::optional<std::string> repository_url;
-
   std::optional<bool> editor;
+  std::optional<bool> from_datadir;
   std::optional<bool> resave;
 
   // std::optional<std::string> locale;